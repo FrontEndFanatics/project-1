8fc3eb1e12c378e401911fe995ecc67562a56bd5
FrontEndFanatics <adityavarshney148@gmail.com>
2024-07-27 00:31:09 +0530

    firstcommit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('orderButton').addEventListener('click', function() {
+    // Assuming you want to switch to a new page called menu.html
+    window.location.href = 'menu.html';
+  });
+  
